0e7d3a92dd5dc7ac92c979d391c8c76d91207ead
Adriano Barbieri <adriano2147@gmail.com>
2020-03-23 20:07:40 +0000

    Badaras

--- lqtgenerator.py
+++ lqtgenerator.py
@@ -1,76 +1,91 @@
 import random
 
-verbo_sem_obj = {
-    0: 'trollou',
-    1: 'feedou',
-    2: 'farmou',
-    3: 'fodeu',
-}
+verbo_sem_obj = [
+    'trollou',
+    'feedou',
+    'farmou',
+    'fodeu',
+]
 
-verbo_clan_uva = {
-    0: 'matar',
-    1: 'mutar',
-}
+verbo_clan_uva = [
+    'matar',
+    'mutar',
+]
 
-moios = {
-    0: 'moios',
-    1: 'broios',
-    2: 'troios',
-    3: 'droios',
-    4: 'trodos',
-    5: 'trotos',
-}
+moios = [
+    'moios',
+    'broios',
+    'troios',
+    'droios',
+    'trodos',
+    'trotos',
+    'badaras',
+]
 
-clan_uva = {
-    0: 'o Adriano',
-    1: 'o Adriano',
-    2: 'o Adriano',
-    3: 'o Adriano',
-    4: 'o Adriano',
-    5: 'o Kibe',
-    6: 'o Pastuch',
-    7: 'o Caio',
-}
+clan_uva = [
+    'o Adriano',
+    'o Adriano',
+    'o Adriano',
+    'o Adriano',
+    'o Adriano',
+    'o Kibe',
+    'o Pastuch',
+    'o Caio',
+    'o Sebben',
+    'o Pedro',
+    'o Dezzo',
+]
 
-especial = {
-    0: 'ó',
-    1: 'EU SOU O LUQUITOOO AHHHH',
-    2: 'tá na jungle farmando hard',
-    3: 'vou aplicar a lei do solinho',
-}
+especial = [
+    'ó',
+    'EU SOU O LUQUITOOO AHHHH',
+    'tá na jungle farmando hard',
+    'vou aplicar a lei do solinho',
+    'Que se foda catequese'
+]
 
-adjetivo_dmais = {
-    0: 'gordo',
-    1: 'carinhas',
-    2: 'gado',
-}
+adjetivo_dmais = [
+    'gordo',
+    'carinhas',
+    'gado',
+    'badaras',
+]
 
-adjetivo_do = {
-    0: 'baby',
-    1: 'xesk',
-    2: 'bresk',
-    3: 'chesque',
-}
+adjetivo_do = [
+    'baby',
+    'xesk',
+    'bresk',
+    'chesque',
+]
 
-adicional = {
-    0: 'papai',
-    1: 'meu papai',
-    2: 'pai',
-    3: 'meu pai',
-}
+adicional = [
+    'papai',
+    'meu papai',
+    'pai',
+    'meu pai',
+    'primo',
+    'meu primo',
+]
+
+
+def frase_pior_que():
+    # pior que daí é (MOIOS)
+    r = random.randint(0, len(moios) - 1)
+    s = moios[r]
+    return str('Pior que daí é ') + str(s)
 
 
 def frase_ai_1():
     # aí (VERBO SEM OBJETO)
     r = random.randint(0, len(verbo_sem_obj) - 1)
-    s = verbo_sem_obj.get(r)
+    s = verbo_sem_obj[r]
     return str('Aí ') + str(s)
 
 
 def frase_ai_2():
     # aí é (MOIOS)
     r = random.randint(0, len(moios) - 1)
-    s = moios.get(r)
+    s = moios[r]
     return str('Aí é ') + str(s)
 
 
@@ -78,28 +93,28 @@ def frase_eu_vou():
     # eu vou (VERBO CLAN UVA) (CLAN UVA)
     r1 = random.randint(0, len(verbo_clan_uva) - 1)
     r2 = random.randint(0, len(clan_uva) - 1)
-    s1 = verbo_clan_uva.get(r1)
-    s2 = clan_uva.get(r2)
+    s1 = verbo_clan_uva[r1]
+    s2 = clan_uva[r2]
     return str('Eu vou ') + str(s1) + ' ' + str(s2)
 
 
 def frase_dmais():
     # (ADJETIVO) d+
     r = random.randint(0, len(adjetivo_dmais) - 1)
-    s = adjetivo_dmais.get(r)
+    s = adjetivo_dmais[r]
     s = str(s) + ' d+'
     return str(frase_adicional(s))
 
 
 def frase_especial():
     r = random.randint(0, len(especial) - 1)
-    s = especial.get(r)
+    s = especial[r]
     return str(s)
 
 
 def frase_do():
     r = random.randint(0, len(adjetivo_do) - 1)
-    s = adjetivo_do.get(r)
+    s = adjetivo_do[r]
     s = str(s) + ' do ' + str(s)
     return str(frase_adicional(s))
 
@@ -110,11 +125,11 @@ def frase_adicional(s):
         return s
     else:
         r = random.randint(0, len(adicional) - 1)
-        return str(s) + ' ' + str(adicional.get(r))
+        return str(s) + ' ' + str(adicional[r])
 
 
 def gera_frase():
-    r = random.randint(0, 100)
+    r = random.randint(0, 120)
     if r <= 20:
         return str(frase_ai_1())
     elif r >= 21 and r <= 40:
@@ -123,9 +138,11 @@ def gera_frase():
         return str(frase_eu_vou())
     elif r >= 61 and r <= 80:
         return str(frase_dmais())
-    elif r >= 81 and r <= 99:
+    elif r >= 81 and r <= 100:
         return str(frase_do())
-    elif r >= 100:
+    elif r >= 101 and r <= 119:
+        return str(frase_pior_que())
+    elif r >= 120:
         return str(frase_especial())
 
 
